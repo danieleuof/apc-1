192ba4808a987527078f61f9d18167107c9514a4
unknown <2612082033@ACADEMICO.IESB.BR>
2026-04-08 09:09:37 -0300

    codigos da pratica02

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main () {
+
+    double duplo = 3.1415926; //ate 12 casas decimais
+    double duplo = 3.1415926;
+    double imprima;
+    printf("%d\n", 3.1415926, imprima);
+    float decimal = 3.1415926f;
+    printf("%f\n", 3.1415926);
+
+    return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main () {
+
+    printf("%i\n", 10);
+    printf("%i\n", -5);
+    printf("%i\n", 00004);
+
+    return 0;
+}
