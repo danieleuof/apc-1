a499d203351a83f8a67638e2735a04506070c6eb
unknown <2612082033@ACADEMICO.IESB.BR>
2026-04-08 10:59:26 -0300

    pratica02

--- praticas/pratica02/imprima_decimais.c
+++ praticas/pratica02/imprima_decimais.c
@@ -1,12 +1,8 @@
 #include <stdio.h> 
 int main () {
 
-    double duplo = 3.1415926; //ate 12 casas decimais
-    double duplo = 3.1415926;
-    double imprima;
-    printf("%d\n", 3.1415926, imprima);
-    float decimal = 3.1415926f;
-    printf("%f\n", 3.1415926);
+    printf("%.7f\n", 3.1415926);
+    printf("%.2f\n", 3.1415926);
 
     return 0;
 }
--- praticas/pratica02/imprima_formatado.c
+++ praticas/pratica02/imprima_formatado.c
@@ -12,5 +12,21 @@ Meia Social     003      19.99
 Total                   229.85
 */
 
+    printf("==============================\n");
+    printf("     N O T A    L E G A L\n");
+    printf("==============================\n");
+    printf("Produto         Qtd Valor Unit\n");
+
+    float quantidade_a1;
+    float valor_a1;
+
+    codigo_a1 = Camiseta;
+    quantidade_a1 = 002f;
+    valor_a1 = 39.99f;
+
+    printf("%0.2f %39.99f\n", quantidade_a1, valor_a1);
+
+    printf("==============================\n");
+
     return 0;
 }
